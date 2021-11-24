409dacab47083a19324cf74d0574f524a9585b76
Georg Bergholz <68726790+georgbergholz@users.noreply.github.com>
2021-11-24 15:05:27 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Syncler-Tutorial'
+copyright = '2021, Sellmore'
+author = 'Georg Bergholz'
 
 release = '0.1'
 version = '0.1.0'
